37f963f37aaf939789371f058c7730dc5819d622
omegazitro <morti137@stu.southtexascollege.edu>
2021-04-04 16:42:14 -0500

    Added Notes Endpoints

---
+++ src/routes/api/index.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import notes from './notes'
+
+const router = Router()
+
+router.get('/', (req, res) => {
+  res.json({ msg: 'API Endpoint' })
+})
+
+router.use('/notes', notes)
+
+export default router
